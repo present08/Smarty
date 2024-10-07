b854084518149aa175eda24de7d4dfe3e38fe8ce
Administrator <present_08@naver.com>
2024-10-07 15:04:06 +0900

    create

---
+++ src/test/java/com/green/smarty/SmartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.smarty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

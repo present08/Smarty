269d118a8f81b6c938eba61a6915149792e1c8dd
2KJ <kincwal@naver.com>
2024-11-01 16:41:43 +0900

    smarty 1,2주차 분석통계 차트

---
+++ smarty_web/src/api/pythonApi.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+const PYTHON_BASE_URL = 'http://localhost:5000/api';
+
+// 수강 인원 현황 데이터 요청
+export const getBarChartData = async () => {
+      const response = await axios.get(`${PYTHON_BASE_URL}/bar-data`);
+      return response.data;
+};
+
+// 시설별 일수입 데이터 요청
+export const getLineChartData = async (queryDate) => {
+    const response = await axios.get(`${PYTHON_BASE_URL}/line-data`, {
+      params: { date: queryDate }
+    });
+    return response.data;
+  };
+  
+// Pie 차트 데이터 요청 (로그인 버전))
+/* export const getPieChartData = async (userId) => {
+      const response = await axios.get(`${PYTHON_BASE_URL}/pie-data/${userId}`);
+      return response.data;
+}; */
+
+// Pie 차트 데이터 요청 (테스트 버전)
+export const getPieChartData = async (userId = 'user33') => {
+  const response = await axios.get(`${PYTHON_BASE_URL}/pie-data`, {
+    params: {user_id : userId}
+  });
+  return response.data;
+};
+
+// 예약현황 데이터 요청
+export const getDayPilotCalendarData = async () => {
+    const response = await axios.get(`${PYTHON_BASE_URL}/daily-scheduler`);
+    return response.data;
+};
+
+// 대여품목 데이터 요청 (관리자용)
+export const getRentalStatistics = async (selectedDate,facilityName) => {
+  const response = await axios.get(`${PYTHON_BASE_URL}/rental-statistics`, {
+      params: { date: selectedDate, facility_name: facilityName },
+  });
+  return response.data;
+};
+
+// 개인별 시설 예약&이용 현황 데이터 요청 (로그인 버전)
+/* export const getPersonallyData = async (userId) => {
+  const response = await axios.get(`${PYTHON_BASE_URL}/personally-data/${userId}`);
+  return response.data;
+}; */
+
+// 개인별 시설 예약&이용 현황 데이터 요청 (테스트 버전)
+export const getPersonallyData = async (userId = 'user33') => {  // 기본 유저 ID 설정
+  const response = await axios.get(`${PYTHON_BASE_URL}/personally-data`, {
+      params: { user_id: userId }
+  });
+  return response.data;
+};
+
+
+// Python 서버로 시설명과 날짜 데이터 전송
+export const sendScheduleData = (facilityName, date) => {
+  return axios.post(`${PYTHON_BASE_URL}/daily-scheduler`, {
+    facility_name: facilityName,
+    date: date
+  });
+};
---
+++ smarty_web/src/chart_components/LineChart.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import HighchartsAccessibility from 'highcharts/modules/accessibility';
+import { getLineChartData } from '../api/pythonApi';
+
+HighchartsAccessibility(Highcharts);  // 접근성 모듈 활성화
+
+  function LineChart({ queryDate }) {  // 날짜를 prop으로 받아옵니다
+    const [chartOptions, setChartOptions] = useState({
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: '수익 차트',  // 제목 초기값
+      },
+      xAxis: {
+        categories: [],  // x축은 시설명
+      },
+      yAxis: {
+        title: {
+          text: '',
+        },
+        labels: {
+          formatter: function() {
+            return this.value / 10000 + '만원';  // Y축 라벨을 만원 단위로 표시
+          },
+        },
+        tickInterval: 50000,  // Y축 눈금을 5만원 단위로 고정
+        min: 0,               // 최소값을 0으로 고정
+        endOnTick: false,      // Y축 값이 딱 맞게 끝나지 않도록 설정
+        maxPadding: 0.1,       // 데이터가 Y축 끝에 붙지 않도록 여백 설정
+      },
+      series: [{
+        name: '수익',   // 시리즈 이름
+        data: [],      // y축 데이터 (수입 값)
+      }],
+      accessibility: {
+        enabled: false,  // 접근성 경고 제거
+      },
+    });
+
+    useEffect(() => {
+      getLineChartData(queryDate).then((response) => {
+        console.log(response.data);  // 데이터 확인
+    
+        const labels = response.data.map(item => item.facility_name);  // x축 카테고리
+        const values = response.data.map(item => item.total_income);   // y축 데이터
+    
+        setChartOptions(prevOptions => ({
+          ...prevOptions,
+          title: {
+            text: `시설별 ${response.date} 수익`,
+          },
+          xAxis: {
+            categories: labels,  
+          },
+          series: [{
+            name: '총액',
+            data: values,  
+          }],
+        }));
+      }).catch(error => {
+        console.error('Error fetching line chart data:', error);
+      });
+    }, [queryDate]);
+
+    return <HighchartsReact highcharts={Highcharts} options={chartOptions} />;
+  }
+
+  export default LineChart;

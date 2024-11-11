8838da64287045c1cde7f0916ed20e12aca5dfb2
2KJ <kincwal@naver.com>
2024-11-11 15:09:04 +0900

    11/11 1차 업데이트

--- smarty_server/src/main/java/com/green/smarty/service/facility/FacilityServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.green.smarty.service.facility;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class FacilityServiceImpl implements FacilityService {
-}

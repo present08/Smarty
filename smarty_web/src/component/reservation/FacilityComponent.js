5ee5f343797aeba21b7ac92d8deaad605b305c4d
김혜수 <>
2024-11-14 16:48:10 +0900

    1114업데이트

--- smarty_web/src/component/reservation/FacilityComponent.js
+++ smarty_web/src/component/reservation/FacilityComponent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import '../../css/facilityComponent.css'
+import '../../css/FacilityComponent.css'
 import { AiOutlineHeart } from 'react-icons/ai'
 
 const FacilityComponent = (props) => {

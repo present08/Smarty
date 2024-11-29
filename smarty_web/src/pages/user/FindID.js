4d30e02552b0a68daaff7ad0bf093f8693bde727
김혜수 <ksykr.313@daum.net>
2024-11-29 12:50:38 +0900

    아이디찾기 css 수정

--- smarty_web/src/pages/user/FindID.js
+++ smarty_web/src/pages/user/FindID.js
@@ -2,6 +2,7 @@ import { tab } from '@testing-library/user-event/dist/tab';
 import axios from 'axios';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import '../../css/findID.css';
 
 const host = 'http://localhost:8080'
 
@@ -26,45 +27,13 @@ const FindID = () => {
 
     return (
 
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#f7f7f7',
-            height: '100vh',
-            width: '100ww'
-        }}>
-            <div style={{
-                display: 'flex',
-                width: '80%',
-                height: '90%',
-                borderRadius: '10px',
-                backgroundColor: 'white',
-                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'
-            }}>
-                <div className="login-container" style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '50%',
-                    paddingTop: '10%',
-                    paddingLeft: '5%',
-                    paddingBottom: '10%'
-                }}>
-                    <h1 style={{
-                        color: '#9db6cf',
-                        fontSize: '40px',
-                        paddingBottom: '20px'
-                    }}>FIND ID</h1>
-
+        <div className='findID_background'>
+            <div className='findID_box'>
+                <div className="findId-container">
+                    <h1>FIND ID</h1>
                     <form onSubmit={handleFindID}>
-                        <div style={{ textAlign: 'left' }}>
-                            <label htmlFor="email" style={{
-                                fontSize: '20px',
-                                color: 'gray',
-                                display: 'block'
-                            }}>EMAIL</label> <br />
+                        <div>
+                            <label htmlFor="email">EMAIL</label> <br />
                             <input
                                 type="text"
                                 id="email"
@@ -72,23 +41,10 @@ const FindID = () => {
                                 onChange={(e) => setEmail(e.target.value)}
                                 placeholder="이메일을 입력하세요."
                                 required
-                                style={{
-                                    width: '400px',
-                                    height: '55px',
-                                    marginBottom: '20px',
-                                    border: 0,
-                                    backgroundColor: '#f2f3f4',
-                                    borderRadius: '10px',
-                                    paddingLeft: '10px'
-                                }}
                             />
                         </div>
-                        <div style={{ textAlign: 'left' }}>
-                            <label htmlFor="user_name" style={{
-                                fontSize: '20px',
-                                color: 'gray',
-                                display: 'block'
-                            }}>USER_NAME</label> <br />
+                        <div>
+                            <label htmlFor="user_name">USER_NAME</label> <br />
                             <input
                                 type="text"
                                 id="user_name"
@@ -96,68 +52,27 @@ const FindID = () => {
                                 onChange={(e) => setUser_name(e.target.value)}
                                 placeholder="이름을 입력하세요."
                                 required
-                                style={{
-                                    width: '400px',
-                                    height: '55px',
-                                    marginBottom: '20px',
-                                    border: 0,
-                                    backgroundColor: '#f2f3f4',
-                                    borderRadius: '10px',
-                                    paddingLeft: '10px'
-                                }}
                             />
                         </div>
-                        <button type="submit" style={{
-                            width: '195px',
-                            height: '45px',
-                            backgroundColor: '#9db6cf',
-                            borderRadius: '10px',
-                            marginTop: '20px',
-                            border: 0,
-                            color: 'white',
-                            fontSize: '18px',
-                            marginRight: '5px',
-                            fontWeight: 'bold'
-                        }}>SEARCH</button>
+                        <button type="submit">SEARCH</button>
                     </form>
                 </div>
-                <div style={{
-                    width: '50%',
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'flex-end',
-                    flexDirection: 'column'
-                }}>
-                    <div style={{
-                        height: '10%'
-                    }}>
-                        <nav
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'space-around', // 메뉴 항목을 좌우로 배치
-                                alignItems: 'center',
-                                padding: '10px 0',
-                            }}
-                        >
+                <div className='findID_submenu_box'>
+                    <div>
+                        <nav>
                             <Link to="/" style={{ textDecoration: 'none', color: 'black', padding: '10px' }}>HOME</Link>
                             <p >&gt;</p>
                             <Link to="/user/login" style={{ textDecoration: 'none', color: 'black', padding: '10px' }}>LOGIN</Link>
                             <p >&gt;</p>
-                            <Link to="/user/findID" style={{ textDecoration: 'none', color: 'black', padding: '10px', marginRight: '10px' }}>FINDID</Link>
-
+                            <Link to="/user/findID" style={{ textDecoration: 'none', color: 'black', padding: '10px', marginRight: '10px', color: '#17468c' }}>FINDID</Link>
                         </nav>
                     </div>
-                    <div style={{
-                        height: '90%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
+                    <div className='findID_image_box' style={{ height: '90%', }}>
                         <img className='img-move' src="/football-team-20.png" alt="이미지" />
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 

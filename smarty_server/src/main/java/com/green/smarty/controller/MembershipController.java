ec8d588743b14e5e45b103378c5d681c18415e9b
김혜수 <ksykr.313@daum.net>
2024-12-05 10:47:24 +0900

    멤버십 6,12월 리셋 구현 완료

--- smarty_server/src/main/java/com/green/smarty/controller/MembershipController.java
+++ smarty_server/src/main/java/com/green/smarty/controller/MembershipController.java
@@ -2,8 +2,10 @@ package com.green.smarty.controller;
 
 import com.green.smarty.dto.TotalMembershipDTO;
 import com.green.smarty.service.UserMembershipService;
+import com.green.smarty.vo.MembershipVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,4 +25,13 @@ public class MembershipController {
         System.out.println("총 결제 금액 : " + totalAmount);
         return totalAmount;
     }
+
+    @GetMapping("/memberGrade")
+    public List<MembershipVO> getUserMemberGrade(@RequestParam("user_id") String user_id) {
+        System.out.println("Received user_id: " + user_id);
+        List<MembershipVO> result = userMembershipService.getUserMembergrade(user_id);
+        return result;
+    }
+
+
 }
